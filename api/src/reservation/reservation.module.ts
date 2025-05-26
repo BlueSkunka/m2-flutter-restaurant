6fe57b28fd4e51642bc23faf9fe6613eb5d39cfc
Olivier PERDRIX <olivierperdrix@live.fr>
2025-05-26 15:37:58 +0200

    feat: add available endpoint

--- api/src/reservation/reservation.module.ts
+++ api/src/reservation/reservation.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CaslModule } from 'nest-casl';
+import { TableEntity } from 'src/tables/entities/table.entity';
+import { TimeSlot } from 'src/time-slots/entities/time-slot.entity';
 import { Reservation } from './entities/reservation.entity';
 import { ReservationController } from './reservation.controller';
 import { permissions } from './reservation.permissions';
 import { ReservationService } from './reservation.service';
 
 @Module({
-  controllers: [ReservationController],
-  providers: [ReservationService],
   imports: [
-    TypeOrmModule.forFeature([Reservation]),
+    TypeOrmModule.forFeature([Reservation, TimeSlot, TableEntity]),
     CaslModule.forFeature({ permissions }),
   ],
-  exports: [ReservationService],
+  controllers: [ReservationController],
+  providers: [ReservationService],
 })
 export class ReservationModule { }
