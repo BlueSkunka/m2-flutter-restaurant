d1e4f4d835ab47548da393d84d62f76b504aeb66
Olivier PERDRIX <olivierperdrix@live.fr>
2025-05-26 17:52:06 +0200

    fix: update reservation model and admin page to match API structure and improve status display

--- app/lib/models/reservation.dart
+++ app/lib/models/reservation.dart
@@ -2,31 +2,42 @@ import 'package:flutter_restaurant_app/models/restaurant_table.dart';
 import 'package:flutter_restaurant_app/models/time_slots.dart';
 
 class Reservation {
-  final String id;
+  final int id;
   final int covers;
-  final String date;
+  final String reservationDate;
   final TimeSlots timeSlot;
   final RestaurantTable restaurantTable;
   final String status;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
   Reservation({
     required this.id,
     required this.covers,
-    required this.date,
+    required this.reservationDate,
     required this.timeSlot,
     required this.restaurantTable,
     required this.status,
+    required this.createdAt,
+    required this.updatedAt,
   });
 
   factory Reservation.fromJson(Map<String, dynamic> json) {
-    print(json);
     return Reservation(
       id: json['id'],
       covers: json['covers'],
-      date: json['reservationDate'],
-      timeSlot: new TimeSlots(startTime: json['timeSlot']['startTime'], endTime: json['timeSlot']['endTime']),
-      restaurantTable: new RestaurantTable(name: json["table"]["name"], capacity: json["table"]["capacity"]),
-      status: json["status"],
+      reservationDate: json['reservationDate'],
+      timeSlot: TimeSlots(
+        startTime: json['timeSlot']['startTime'],
+        endTime: json['timeSlot']['endTime']
+      ),
+      restaurantTable: RestaurantTable(
+        name: json['table']['name'],
+        capacity: json['table']['capacity']
+      ),
+      status: json['status'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
     );
   }
 }
